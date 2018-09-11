4674b378096a86f2fb79768073a1fc34ccb3aa93
Максим Тарасов <yousan4ik@gmail.com>
2018-09-11 19:53:56 +0300

    upd

--- src/php/android/framework/activity/AbstractActivity.php
+++ src/php/android/framework/activity/AbstractActivity.php
@@ -5,6 +5,9 @@ namespace php\android\framework\activity;
 use php\android\UXAppBar;
 use php\android\UXToast;
 use php\android\UXView;
+use php\gui\UXLoader;
+use php\io\Stream;
+use php\lib\str;
 
 abstract class AbstractActivity extends UXView
 {
@@ -18,10 +21,10 @@ abstract class AbstractActivity extends UXView
      */
     abstract public function onUpdateAppBar(UXAppBar $appBar) : void;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct($this->getName() ?? "home");
         $this->setOnUpdateAppBar([$this, "onUpdateAppBar"]);
+        $this->loadFXML(str::replace(get_class($this), "\\", "/") . ".fxml");
     }
 
     /**
@@ -29,13 +32,26 @@ abstract class AbstractActivity extends UXView
      * @param int $duration
      * @return UXToast
      */
-    public function toast(string $msg, int $duration = 1000) : UXToast
-    {
+    public function toast(string $msg, int $duration = 1000) : UXToast {
         $toast = new UXToast();
         $toast->text = $msg;
         $toast->duration = $duration;
         $this->show();
 
         return $toast;
+    }
+
+    /**
+     * @param string $path
+     */
+    protected function loadFXML(string $path)
+    {
+        Stream::tryAccess($path, function (Stream $stream) {
+            try {
+                $this->center = (new UXLoader())->load($stream);
+            } catch (\Exception $exception) {
+                echo "Error load from fxml: {$exception->getMessage()}\n";
+            }
+        });
     }
 }
